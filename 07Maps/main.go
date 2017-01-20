a908cf008e32f1bd4b35fc1a6e3c7e489ede2b74
Michael Roberts <mikeyrobby81@gmail.com>
2017-01-20 13:55:27 -0800

    Made it look better and added a loop

--- 07Maps/main.go
+++ 07Maps/main.go
@@ -5,19 +5,25 @@ import "fmt"
 func main() {
   // creating a map of authors and one of their books
   m := map[string]string{
-    "Miguel de Cervantes" : "Don Quixote" ,
-    "Charles Dickens" : "A Tale of Two Cities" ,
-    "J.R.R. Tolkien" : "The Lord of the Rings" ,
-    "Antoine de Saint-Exupery" : "The Little Prince" ,
-    "J.K. Rowling" : "Harry Potter and the Sorcerer's Stone" ,
-    "Agatha Christie" : "And Then There Were None" ,
-    "Cao Xueqin" : "The Dream of the Red Chamber" ,
-    "H. Rider Haggard" : "She: A History of Adventure" ,
-    "C.S. Lewis" : "The Lion, the Witch and the Wardrobe" ,
-    "Dan Brown" : "The Da Vinci Code" ,
+    "Miguel de Cervantes: " : "Don Quixote" ,
+    "Charles Dickens: " : "A Tale of Two Cities" ,
+    "J.R.R. Tolkien: " : "The Lord of the Rings" ,
+    "Antoine de Saint-Exupery: " : "The Little Prince" ,
+    "J.K. Rowling: " : "Harry Potter and the Sorcerer's Stone" ,
+    "Agatha Christie: " : "And Then There Were None" ,
+    "Cao Xueqin: " : "The Dream of the Red Chamber" ,
+    "H. Rider Haggard: " : "She: A History of Adventure" ,
+    "C.S. Lewis: " : "The Lion, the Witch and the Wardrobe" ,
+    "Dan Brown: " : "The Da Vinci Code" ,
   }
  // Prints out a single entry from the map with a key of J.K. Rowling
   fmt.Println(m["J.K. Rowling"])
   // prints out the entire map
   fmt.Println(m)
+
+  // Looping through the map and printing out the key and value
+  for k, v := range m {
+    fmt.Println(k,v)
+
+  }
 }
