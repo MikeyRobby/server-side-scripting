d46017765336c87f3188e6141e1d903b93e7f672
Michael Roberts <mikeyrobby81@gmail.com>
2017-01-20 13:47:34 -0800

    Update: added info

---
+++ 07Maps/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+  // creating a map of authors and one of their books
+  m := map[string]string{
+    "Miguel de Cervantes" : "Don Quixote" ,
+    "Charles Dickens" : "A Tale of Two Cities" ,
+    "J.R.R. Tolkien" : "The Lord of the Rings" ,
+    "Antoine de Saint-Exupery" : "The Little Prince" ,
+    "J.K. Rowling" : "Harry Potter and the Sorcerer's Stone" ,
+    "Agatha Christie" : "And Then There Were None" ,
+    "Cao Xueqin" : "The Dream of the Red Chamber" ,
+    "H. Rider Haggard" : "She: A History of Adventure" ,
+    "C.S. Lewis" : "The Lion, the Witch and the Wardrobe" ,
+    "Dan Brown" : "The Da Vinci Code" ,
+  }
+
+  fmt.Println(m["J.K. Rowling"])
+  fmt.Println(m)
+}
