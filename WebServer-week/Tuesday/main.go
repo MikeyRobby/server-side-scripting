3e71f3195d18aac6171fbbf4de8a1118fa36ee5c
root <root@localhost.localdomain>
2017-02-14 19:54:41 +0000

    Tuesday Web Server

---
+++ WebServer-week/Tuesday/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+var tpl *template.Template
+
+func init(){
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main(){
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request){
+	tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
