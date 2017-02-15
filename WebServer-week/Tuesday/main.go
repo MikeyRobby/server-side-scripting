993b5d7c7e2f5edfade267867108e8c6c9e1bdfa
Michael Roberts <mikeyrobby81@gmail.com>
2017-02-14 23:22:17 -0800

    Updated Tuesday Web Server

--- WebServer-week/Tuesday/main.go
+++ WebServer-week/Tuesday/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "io/ioutil"
 	"net/http"
 	"html/template"
 )
@@ -13,10 +14,16 @@ func init(){
 
 func main(){
 	http.HandleFunc("/", index)
+  http.HandleFunc("/blog", blog)
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, req *http.Request){
-	tpl.ExecuteTemplate(w, "index.html", nil)
+  tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func blog(w http.ResponseWriter, req *http.Request){
+  r, _ := ioutil.ReadFile("posts/test")
+  tpl.ExecuteTemplate(w, "blog.html", string(r))
 }
 
